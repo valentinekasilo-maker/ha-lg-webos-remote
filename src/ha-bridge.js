bf6014f2b7842e8b2d17412ef39b241f5095ce58
valentinekasilo-maker <valentinekasilo@gmail.com>
2026-09-06 02:31:44 -0700

    fix(mqtt): unregister legacy number button entities and streamline dedicated MQTT control topics

--- src/ha-bridge.js
+++ src/ha-bridge.js
@@ -318,6 +318,11 @@ class HomeAssistantBridge {
       });
     });
 
+    // Unregister legacy number buttons (0-9, dash) from MQTT broker so Home Assistant removes them
+    ['num_0', 'num_1', 'num_2', 'num_3', 'num_4', 'num_5', 'num_6', 'num_7', 'num_8', 'num_9', 'num_dash'].forEach((id) => {
+      this.publish(`${prefix}/button/${deviceId}/${id}/config`, '', { retain: true });
+    });
+
     // 4. Select Source
     this.publish(`${prefix}/select/${deviceId}/source/config`, {
       name: 'Input Source',
